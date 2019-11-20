615410e3a66c358dbcdecb0698b80b59baddea8a
MANDARIN <compurain@naver.com>
2019-11-20 21:47:32 +0900

    add totalPrice

--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -2,6 +2,7 @@
 	<div>
 		<h3>카트 리스트</h3>
 		<CartItem v-for="item of globalCarts" :key="item.id" :item="item" />
+		<h3>TOTAL {{ totalPrice }} 원</h3>
 	</div>
 </template>
 
@@ -15,6 +16,13 @@ export default {
 	computed: {
 		globalCarts() {
 			return this.$store.getters.globalCarts;
+		},
+		totalPrice() {
+			const total = this.globalCarts.reduce(
+				(acc, item) => (acc += item.price * item.count),
+				0
+			);
+			return total;
 		},
 	},
 };
