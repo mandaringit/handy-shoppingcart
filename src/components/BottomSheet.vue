1afdef55300f518399afda2daba7a3f460baabb4
MANDARIN <compurain@naver.com>
2019-12-12 08:43:45 +0900

    스크롤링 가능하게 수정

--- src/components/BottomSheet.vue
+++ src/components/BottomSheet.vue
@@ -1,41 +1,39 @@
 <template>
-	<div class="text-center">
-		<v-bottom-sheet v-model="sheet">
-			<template #activator="{on}">
-				<v-btn v-on="on" text icon color="red">
-					<v-icon>
-						mdi-plus
-					</v-icon>
-				</v-btn>
-			</template>
-			<v-sheet class="text-center">
-				<div style="position:relative;height:4rem">
-					<v-btn @click="closeSheet" class="mt-4" absolute right text>
-						<v-icon>mdi-close</v-icon>
-					</v-btn>
-				</div>
-				<ItemInput @itemAdded="closeSheet" />
-			</v-sheet>
-		</v-bottom-sheet>
-	</div>
+  <div class="text-center">
+    <v-bottom-sheet v-model="sheet">
+      <template #activator="{on}">
+        <v-btn v-on="on" text icon color="red">
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </template>
+      <v-sheet class="text-center overflow-y-auto" max-height="600px">
+        <div style="position:relative;height:2rem">
+          <v-btn @click="closeSheet" class="mt-4" absolute right text>
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </div>
+        <ItemInput @itemAdded="closeSheet" />
+      </v-sheet>
+    </v-bottom-sheet>
+  </div>
 </template>
 
 <script>
-import ItemInput from './ItemInput';
+import ItemInput from "./ItemInput";
 export default {
-	data() {
-		return {
-			sheet: false,
-		};
-	},
-	components: {
-		ItemInput,
-	},
-	methods: {
-		closeSheet() {
-			this.sheet = !this.sheet;
-		},
-	},
+  data() {
+    return {
+      sheet: false
+    };
+  },
+  components: {
+    ItemInput
+  },
+  methods: {
+    closeSheet() {
+      this.sheet = !this.sheet;
+    }
+  }
 };
 </script>
 
