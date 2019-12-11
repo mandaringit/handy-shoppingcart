931991f1511c2248da2c1b5bf2de9568f848a44b
MANDARIN <compurain@naver.com>
2019-12-12 08:34:08 +0900

    footer 추가

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,90 @@
 <template>
-	<v-app>
-		<div class="overflow-hidden">
-			<v-app-bar
-				color="deep-purple accent-4"
-				absoulute
-				dark
-				scroll-target="#app-contents"
-				fixed
-			>
-				<v-toolbar-title class="app-title">HANDY SHOPPING CART</v-toolbar-title>
-				<v-spacer class="wide-spacer">
-					<!-- 화면너비가 600px보다 클때 나타납니다 -->
-				</v-spacer>
-				<v-toolbar-title class="mr-5">{{
-					totalPrice | numbering
-				}}</v-toolbar-title>
-				<v-spacer class="small-spacer">
-					<!-- 화면너비가 600px 보다 작아지면 나타납니다 -->
-				</v-spacer>
-				<BottomSheet />
-			</v-app-bar>
-		</div>
-		<v-sheet id="app-contents" class="over-flow-y-auto">
-			<CartList />
-		</v-sheet>
-	</v-app>
+  <v-app>
+    <!-- nave -->
+    <div class="overflow-hidden">
+      <v-app-bar color="deep-purple accent-4" absoulute dark scroll-target="#app-contents" fixed>
+        <v-toolbar-title class="app-title">HANDY SHOPPING CART</v-toolbar-title>
+        <v-spacer class="wide-spacer">
+          <!-- 화면너비가 600px보다 클때 나타납니다 -->
+        </v-spacer>
+        <v-toolbar-title class="mr-5">
+          {{
+          totalPrice | numbering
+          }}
+        </v-toolbar-title>
+        <v-spacer class="small-spacer">
+          <!-- 화면너비가 600px 보다 작아지면 나타납니다 -->
+        </v-spacer>
+        <BottomSheet />
+      </v-app-bar>
+    </div>
+    <!-- content -->
+    <v-sheet id="app-contents" class="over-flow-y-auto">
+      <CartList />
+    </v-sheet>
+    <!-- footer -->
+    <v-footer absolute>
+      <v-spacer></v-spacer>
+      <div>MANDARIN</div>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import CartList from './components/CartList';
-import BottomSheet from './components/BottomSheet';
-import { mapGetters } from 'vuex';
-import numeral from 'numeral';
+import CartList from "./components/CartList";
+import BottomSheet from "./components/BottomSheet";
+import { mapGetters } from "vuex";
+import numeral from "numeral";
 export default {
-	name: 'app',
-	components: {
-		CartList,
-		BottomSheet,
-	},
-	mounted() {
-		this.$store.dispatch('initialize');
-	},
-	computed: {
-		...mapGetters(['globalCarts']),
-		totalPrice() {
-			const total = this.globalCarts.reduce(
-				(acc, item) => (acc += item.price * item.count),
-				0
-			);
-			return total;
-		},
-	},
-	filters: {
-		numbering(value) {
-			return `TOTAL ₩ ${numeral(value).format('0,0')}`;
-		},
-	},
+  name: "app",
+  components: {
+    CartList,
+    BottomSheet
+  },
+  mounted() {
+    this.$store.dispatch("initialize");
+  },
+  computed: {
+    ...mapGetters(["globalCarts"]),
+    totalPrice() {
+      const total = this.globalCarts.reduce(
+        (acc, item) => (acc += item.price * item.count),
+        0
+      );
+      return total;
+    }
+  },
+  filters: {
+    numbering(value) {
+      return `TOTAL ₩ ${numeral(value).format("0,0")}`;
+    }
+  }
 };
 </script>
 
 <style>
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
 }
 
 .small-spacer {
-	display: none;
+  display: none;
 }
 
 @media (max-width: 600px) {
-	.app-title {
-		display: none;
-	}
+  .app-title {
+    display: none;
+  }
 
-	.wide-spacer {
-		display: none;
-	}
+  .wide-spacer {
+    display: none;
+  }
 
-	.small-spacer {
-		display: inline;
-	}
+  .small-spacer {
+    display: inline;
+  }
 }
 </style>
